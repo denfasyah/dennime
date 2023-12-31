4e43b1009cdd73a24f7abe65b0101fa87c539e5e
denfasyah <denfasyah@gmail.com>
2023-12-31 15:12:29 +0700

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        Grey: "#222430",
+        Purple: "#bf8cfa",
+        White: "#fefffd",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
