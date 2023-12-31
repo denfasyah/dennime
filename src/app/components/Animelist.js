957e3c2fe962d3f778171a7ed6740c655096826d
denfasyah <denfasyah@gmail.com>
2023-12-31 16:13:10 +0700

    fetch data anime

--- src/app/components/Animelist.js
+++ src/app/components/Animelist.js
@@ -1,51 +1,15 @@
 import Image from "next/image";
 
-const Animelist = () => {
+const Animelist = ({title,images}) => {
   return (
-    <div className="grid grid-cols-3 gap-2 h-32  mx-10">
       <div className="bg-purple-800">
         <Image
-          src="https://placehold.co/600x400/png"
+          src={images}
           width={600}
           height={600}
-        /> <h3>title anime</h3>
+          alt="..."
+        /> <h3>{title}</h3>
       </div>
-      <div className="bg-purple-800">
-        <Image
-          src="https://placehold.co/600x400/png"
-          width={600}
-          height={600}
-        />
-      </div>
-      <div className="bg-purple-800">
-        <Image
-          src="https://placehold.co/600x400/png"
-          width={600}
-          height={600}
-        />
-      </div>
-      <div className="bg-purple-800">
-        <Image
-          src="https://placehold.co/600x400/png"
-          width={600}
-          height={600}
-        />
-      </div>
-      <div className="bg-purple-800">
-        <Image
-          src="https://placehold.co/600x400/png"
-          width={600}
-          height={600}
-        />
-      </div>
-      <div className="bg-purple-800">
-        <Image
-          src="https://placehold.co/600x400/png"
-          width={600}
-          height={600}
-        />
-      </div>
-    </div>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 import Animelist from "./components/Animelist";
 
-const Home = () => {
+const Home = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+  );
+  const anime = await response.json();
+  console.log(anime);
   return (
     <div>
       <Navbar />
-      <h1 className='text-2xl ml-10 my-10 text-White font-medium'>Popular</h1>
-      <Animelist />
+      <h1 className="text-2xl ml-10 my-10 text-White font-medium">Popular</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {anime.data.map((data) => {
+          return (
+            <Animelist title={data.title} images={data.images.webp.image_url} />
+          );
+        })}
+      </div>
     </div>
   );
 };
